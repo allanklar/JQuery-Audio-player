de1d8498039cc8ec4f190cf90eefaa5a2a469486
Allan klar <allanklar@hotmail.com>
2019-10-08 16:42:38 -0500

    assignment.blocjams-sliders:completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -52,7 +52,7 @@
 
         const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
         const previousSongIndex = currentSongIndex - 1;
-        if (previousSongIndex < -1) { return; }
+        if (previousSongIndex == -1) { return; }
 
         const previousSong = album.songs[previousSongIndex];
         helper.playPauseAndUpdate(previousSong);
